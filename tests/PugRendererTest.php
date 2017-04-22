b18e78d2966e58f991678773cde689240e6c6532
Benjamin Franzke <benjaminfranzke@gmail.com>
2017-04-22 11:01:47 +0200

    Test PugRenderer::__invoke

--- tests/PugRendererTest.php
+++ tests/PugRendererTest.php
@@ -1,6 +1,7 @@
 <?php
 use Slim\Http\Body;
 use Slim\Http\Headers;
+use Slim\Http\Request;
 use Slim\Http\Response;
 
 class PugRendererTest extends PHPUnit_Framework_TestCase
@@ -59,6 +60,27 @@ class PugRendererTest extends PHPUnit_Framework_TestCase
         $this->assertEquals("Hi", $newResponse->getBody()->getContents());
     }
 
+    public function testMiddlewareInvoke() {
+        $renderer = new \Bnf\PugView\PugRenderer([
+            'extension' => '.pug',
+            'basedir' => 'tests/templates/'
+        ]);
+
+        $headers = new Headers();
+        $body = new Body(fopen('php://temp', 'r+'));
+        $response = new Response(200, $headers, $body);
+        $request = $this->mockRequest();
+
+        $renderer->__invoke($request, $response, function($request, $response) {
+            return $response;
+        });
+
+        $renderer->set('hello', 'Hi');
+        $newResponse = $renderer->render('test');
+        $newResponse->getBody()->rewind();
+        $this->assertEquals("Hi", $newResponse->getBody()->getContents());
+    }
+
     public function testExceptionInTemplate() {
         $renderer = new \Bnf\PugView\PugRenderer([
             'extension' => '.pug',
@@ -103,5 +125,25 @@ class PugRendererTest extends PHPUnit_Framework_TestCase
         $response = new Response(200, $headers, $body);
 
         $renderer->render('adfadftestTemplate', [], $response);
+    }
+
+    /**
+     * @return \Slim\Http\Request
+     */
+    protected function mockRequest()
+    {
+        $env = \Slim\Http\Environment::mock([
+            'SCRIPT_NAME' => '/index.php',
+            'REQUEST_URI' => '/',
+            'REQUEST_METHOD' => 'GET',
+        ]);
+        $uri = \Slim\Http\Uri::createFromEnvironment($env);
+        $headers = \Slim\Http\Headers::createFromEnvironment($env);
+        $cookies = [];
+        $serverParams = $env->all();
+        $body = new \Slim\Http\RequestBody();
+        $req = new \Slim\Http\Request('GET', $uri, $headers, $cookies, $serverParams, $body);
+
+        return $req;
     }
 }
